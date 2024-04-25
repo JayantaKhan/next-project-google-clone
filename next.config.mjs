64fe441261e2debb64d8bf9675a6c47dc32cd926
JayantaKhan <jayantakhan06@gmail.com>
2024-04-25 11:33:23 +0530

    add files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "www.google.com"],
+  },
+};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       "https://images.unsplash.com/*",
+//       "https://www.google.com/*",
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// // next.config.mjs
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       "https://images.unsplash.com/.*", // Note: added regex pattern
+//       "https://www.google.com/.*", // Note: added regex pattern
+//     ],
+//   },
+// };
+
+// export default nextConfig;
